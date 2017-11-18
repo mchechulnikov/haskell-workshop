5769ba757a1dba4dd8eb3583b5a83d12ac1f6822
sunloving <ilfirin.nar@gmail.com>
2017-11-18 17:19:31 +0300

    stepic course: 1.5: guards (2)

--- stepic/1.5/guards.hs
+++ stepic/1.5/guards.hs
@@ -1,5 +1,6 @@
 module Demo where
     
 factorial' 0 = 1
-factorial' n | n < 0 = error "arg must be >= 0"
-             | n > 0 = n * factorial' (n - 1)
+factorial' n | n == 0   = 1
+             | n < 0    = error "arg must be >= 0"
+             | n > 0    = n * factorial' (n - 1)
